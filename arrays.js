2226f880502302f77b7368d3a3fdf9362804caa0
Amit Kumar <56325341+Amit-K-Dev@users.noreply.github.com>
2026-01-13 20:55:32 +0530

    Practice basic JavaScript arrays

---
+++ arrays.js
@@ -0,0 +1,13 @@
+// JavaScript Arrays Practice
+
+// Creating an array
+const numbers = [1, 2, 3, 4, 5];
+
+// Accessing elements
+console.log(numbers[0]);   // first element
+console.log(numbers[4]);   // last element
+
+// Looping through array
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
