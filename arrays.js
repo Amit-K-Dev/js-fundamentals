cc1ea0023c8b75e35cc5d81bd705e42b52d7453b
Amit Kumar <56325341+Amit-K-Dev@users.noreply.github.com>
2026-01-14 19:29:13 +0530

    Practice basic JavaScript array methods

--- arrays.js
+++ arrays.js
@@ -10,4 +10,17 @@ console.log(numbers[4]);   // last element
 // Looping through array
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
-}
+};
+
+// Array methods practice
+
+// Adding an element
+numbers.push(6);
+console.log(numbers);
+
+// Removing the last element
+numbers.pop();
+console.log(numbers);
+
+// Array length
+console.log("Array length:", numbers.length);
